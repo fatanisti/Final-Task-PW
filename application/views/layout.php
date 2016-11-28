ac5fde42fbdfcac5a03f6369e41d2495bd71eb34
Dwiki Nurkurniawan <nurkurniawandwiki@gmail.com>
2016-11-28 12:22:19 +0700

    first commit beb :*

---
+++ application/views/layout.php
@@ -0,0 +1,51 @@
+<div class="container">    
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">menswear</div></a>
+        <div class="panel-body"><a href='menswear.html'><img src="" class="img-responsive" style="width:100%" alt="Image"></div></a>
+        <div class="panel-footer">Buy 3 get 1 free</div>
+      </div>
+    </div>
+    <div class="col-sm-4"> 
+      <div class="panel panel-danger">
+        <div class="panel-heading">womenswear</div>
+        <div class="panel-body"><a href='womenswear.html'><img src="" class="img-responsive" style="width:100%" alt="Image"></div></a>
+        <div class="panel-footer">Buy 3 get 1 free</div>
+      </div>
+    </div>
+    <div class="col-sm-4"> 
+      <div class="panel panel-success">
+        <div class="panel-heading">dress</div>
+        <div class="panel-body"><a href='dress.html'><img src="" class="img-responsive" style="width:100%" alt="Image"></div></a>
+        <div class="panel-footer">Buy 3 get 1 free</div>
+      </div>
+    </div>
+  </div>
+</div><br>
+
+<div class="container">    
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="panel panel-info">
+        <div class="panel-heading">shoes for men</div>
+        <div class="panel-body"><a href='shoesformen.html'><img src="" class="img-responsive" style="width:100%" alt="Image"></div></a>
+        <div class="panel-footer">Buy 3 get 1 free</div>
+      </div>
+    </div>
+    <div class="col-sm-4"> 
+      <div class="panel panel-warning">
+        <div class="panel-heading">shoes for women</div>
+        <div class="panel-body"><a href='shoesforwomen'><img src="" class="img-responsive" style="width:100%" alt="Image"></div></a>
+        <div class="panel-footer">Buy 3 get 1 free</div>
+      </div>
+    </div>
+    <div class="col-sm-4"> 
+      <div class="panel panel-danger">
+        <div class="panel-heading">accessories</div>
+        <div class="panel-body"><a href='accessories'><img src="" class="img-responsive" style="width:100%" alt="Image"></div></a>
+        <div class="panel-footer">Buy 3 get 1 free</div>
+      </div>
+    </div>
+  </div>
+</div><br><br>

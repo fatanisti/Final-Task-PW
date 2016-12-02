d3d693c0e27b99a06bcd9508ed8875063fa8c7e6
fatanisti <fatanaufa97@gmail.com>
2016-12-02 15:14:24 +0700

    Tambahan database buat items

---
+++ pjs_items.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 02 Des 2016 pada 09.13
+-- Versi Server: 10.1.13-MariaDB
+-- PHP Version: 7.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pjs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pjs_items`
+--
+
+CREATE TABLE `pjs_items` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `kategori` varchar(30) NOT NULL,
+  `namaBarang` varchar(30) NOT NULL,
+  `hargaBarang` varchar(30) NOT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pjs_items`
+--
+ALTER TABLE `pjs_items`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pjs_items`
+--
+ALTER TABLE `pjs_items`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

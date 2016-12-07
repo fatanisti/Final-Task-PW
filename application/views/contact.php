72e6c31361527c2c93a8b229e0885792e9f46159
ariebrian <lightflame0@gmail.com>
2016-12-07 12:16:20 +0700

    Add files via upload

--- application/views/contact.php
+++ application/views/contact.php
@@ -3,15 +3,15 @@
     <div class="col-sm-6">
       <div class="panel panel-primary">
         <div class="panel-heading" align="center">Dwiki Nurkurniawan (140810150014)</div></a>
-        <div class="panel-body"><img src="dwiki.jpg" class="img-responsive" width="400px" height="600px" alt="Image"></div>
-        <div class="panel-footer"></div>
+        <div class="panel-body" style="max-height: 300px; min-height: 300px"><img src="<?php echo base_url(); ?>assets/img/dwiki.jpg" class="img-responsive" width="100%" height="100%" style="max-height: 300px; position: relative; overflow: hidden" alt="Image"></div>
+        <div class="panel-footer" style="margin-top: 20px"></div>
       </div>
     </div>
     <div class="col-sm-6"> 
       <div class="panel panel-primary">
         <div class="panel-heading" align="center">Vina Nur Fitriani (140810150052)</div>
-        <div class="panel-body"><img src="vina.jpg" class="img-responsive" width="400px" height="600px" alt="Image"></div>
-        <div class="panel-footer"></div>
+        <div class="panel-body" style="max-height: 300px; min-height: 300px"><img src="<?php echo base_url(); ?>assets/img/vina.jpg" class="img-responsive" width="100%" height="100%" style="max-height: 300px; position: relative; overflow: hidden" alt="Image"></div>
+        <div class="panel-footer" style="margin-top: 20px"></div>
       </div>
     </div>
   </div>
@@ -19,15 +19,15 @@
     <div class="col-sm-6">
       <div class="panel panel-primary">
         <div class="panel-heading" align="center">Arie Brian (140810150058)</div>
-        <div class="panel-body"><img src="arie.jpg" class="img-responsive" width="400px" height="600px" alt="Image"></div>
-        <div class="panel-footer"></div>
+        <div class="panel-body" style="max-height: 300px; min-height: 300px"><img src="<?php echo base_url(); ?>assets/img/arie.jpg" class="img-responsive" width="100%" height="100%" style="max-height: 300px; position: relative; overflow: hidden" alt="Image"></div>
+        <div class="panel-footer" style="margin-top: 20px"></div>
       </div>
     </div>
     <div class="col-sm-6"> 
       <div class="panel panel-primary">
         <div class="panel-heading" align="center">Fatan Aufa (140810150064)</div>
-        <div class="panel-body"><img src="fatan.jpg" class="img-responsive" width="400px" height="600px" alt="Image"></div>
-        <div class="panel-footer"></div>
+        <div class="panel-body" style="max-height: 300px; min-height: 300px"><img src="<?php echo base_url(); ?>assets/img/fatan.jpg" class="img-responsive" width="100%" height="100%" style="max-height: 300px; position: relative; overflow: hidden" alt="Image"></div>
+        <div class="panel-footer" style="margin-top: 20px"></div>
       </div>
     </div>
   </div>
